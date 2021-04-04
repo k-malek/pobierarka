a1d6ddb8fa433f067feaa4d1206261c2978c897e
k-malek <47522296+k-malek@users.noreply.github.com>
2021-04-04 20:45:31 +0200

    Mainly cosmetic
    > remove unused class Timer
    > remove unused variables
    > add comments
    > add quasi overloading on pobier
    > translate to english

--- pobierarka.py
+++ pobierarka.py
@@ -3,39 +3,26 @@ from time import *
 import urllib.request
 import re
 
-class Timer:
-	def __init__(self,remaining):
-		self.remaining=0
-	def __enter__(self):
-		self.start=time.now()
-		return self
-	
+#download single vid
 def pobier(lunk):
+	if 'youtube' not in lunk:
+		lunk = 'https://www.youtube.com/watch?v='+lunk
 	try:
-		global all
-		global elapsed_time
-		global now
-		all=0
-		elapsed_time=0
-		now=time()
-		vid=YouTube('https://www.youtube.com/watch?v='+lunk, on_progress_callback=progress_Check)
-		print("Oto pobieram Ci:",vid.title)
-		strim=vid.streams.get_highest_resolution()
-		strim.download()
+		vid=YouTube(lunk, on_progress_callback=progress_check)
+		print("I download for U:",vid.title)
+		strim=vid.streams.get_highest_resolution().download()
 		print("100.00%")
 	except Exception as e:
-		print("Nie pykuo :<")
+		print("Somethin went horribly rong :<")
 		print(e)
-		
+
+#download playlist		
 def pobier_liste(link_listy, ile=1000):
-	x = urllib.request.urlopen(link_listy)
-	x = x.read()
-	x = re.findall(r'"videoId":"([^"]*)"',x.decode("utf-8"))
-	x = set(x)
-	i=0
+	x = urllib.request.urlopen(link_listy).read()
+	x = set(re.findall(r'"videoId":"([^"]*)"',x.decode("utf-8")))
 	all_vid=len(x)
 	pobrane=[]
-	for vid in x:
+	for vid,i in enumerate(x):
 		print(str(i+1)+"/"+str(all_vid),end=" ")
 		if vid in pobrane:
 			continue
@@ -44,12 +31,9 @@ def pobier_liste(link_listy, ile=1000):
 			pobrane.append(vid)
 		else: 
 			break
-		i+=1
-		
-def progress_Check(stream = None, chunk = None, file_handle = None, remaining = None):
-	global all
-	global elapsed_time
-	global now
+
+#progress percentage visualization
+def progress_check(stream = None, chunk = None, file_handle = None, remaining = None):
 	all+=len(chunk)
 	elapsed_time+=time()-now
 	now=time()
@@ -58,5 +42,4 @@ def progress_Check(stream = None, chunk = None, file_handle = None, remaining = 
 	hours=int(remaining_time/3600)
 	minutes=int((remaining_time-hours*3600)/60)
 	seconds=int(remaining_time-hours*3600-minutes*60)
-	print("{:.2f}% upłynęło {}s pozostało ok. {}h {}min {}s         ".format(percent,int(elapsed_time),hours,minutes,seconds), end='\r')
-	
+	print("{:.2f}% passed {}s, ETA app. {}h {}min {}s         ".format(percent,int(elapsed_time),hours,minutes,seconds), end='\r')
