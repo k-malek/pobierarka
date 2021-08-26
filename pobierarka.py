f52cfa924e13b54ab449c8bd2d6c738a4fc3c72a
k-malek <krzysztof.malek.90@gmail.com>
2021-08-26 22:40:57 +0200

    adding downloading vids from channel

--- pobierarka.py
+++ pobierarka.py
@@ -3,52 +3,66 @@ from time import *
 import urllib.request, re, math
 
 #download single vid
-def pobier(lunk):
-	global all
-	global elapsed_time
-	global now
-	all=0
-	elapsed_time=0
-	now=time()
-	if 'youtube' not in lunk:
-		lunk = 'https://www.youtube.com/watch?v='+lunk
-	try:
-		vid=YouTube(lunk, on_progress_callback=progress_check)
-		print("I download for U:",vid.title)
-		strim=vid.streams.get_highest_resolution().download()
-		print("100.00%")
-	except Exception as e:
-		print("Somethin went horribly rong :<")
-		print(e)
+def pobier(lunk,path=''):
+    global all
+    global elapsed_time
+    global now
+    all=0
+    elapsed_time=0
+    now=time()
+    if 'youtube' not in lunk:
+        lunk = 'https://www.youtube.com/watch?v='+lunk
+    try:
+        vid=YouTube(lunk, on_progress_callback=progress_check)
+        print("I download for U:",vid.title)
+        if(path==''):
+            strim=vid.streams.get_highest_resolution().download()
+        else:
+            strim=vid.streams.get_highest_resolution().download(path)
+        print("100.00%")
+    except Exception as e:
+        print("Somethin went horribly rong :<")
+        print(e)
 
-#download playlist		
+#download playlist        
 def pobier_liste(link_listy, ile=1000):
-	x = urllib.request.urlopen(link_listy).read()
-	x = re.findall(r'"videoId":"([^"]*)"',x.decode("utf-8"))
-	all_vid=len(x)
-	pobrane=[]
-	for i,vid in enumerate(x):
-		print(str(i+1)+"/"+str(all_vid),end=" ")
-		if vid in pobrane:
-			continue
-		if i<int(ile): 
-			pobier(vid)
-			pobrane.append(vid)
-		else: 
-			break
+    x = urllib.request.urlopen(link_listy).read()
+    x = re.findall(r'"videoId":"([^"]*)"',x.decode("utf-8"))
+    all_vid=len(x)
+    pobrane=[]
+    for i,vid in enumerate(x):
+        print(str(i+1)+"/"+str(all_vid),end=" ")
+        if vid in pobrane:
+            continue
+        if i<int(ile): 
+            pobier(vid)
+            pobrane.append(vid)
+        else: 
+            break
 
+#download videos from channel            
+def pobier_kanal(channel_name,ile=5):
+    c=Channel('https://www.youtube.com/c/'+channel_name)
+    i=0
+    for vid in c.video_urls:
+        print(vid)
+        pobier(vid)
+        i+=1
+        if i==ile:
+            break
+            
 #progress percentage visualization
 def progress_check(stream = None, chunk = None, file_handle = None, remaining = None):
-	global all
-	global elapsed_time
-	global now
-	all+=len(chunk)
-	elapsed_time+=time()-now
-	now=time()
-	percent=all/stream.filesize*100
-	remaining_time=(elapsed_time*100/percent)*((100-percent)/100)
-	hours=int(remaining_time/3600)
-	minutes=int((remaining_time-hours*3600)/60)
-	seconds=int(remaining_time-hours*3600-minutes*60)
-	strokes_for_bar=math.floor(percent//5)
-	print("{:.2f}% [".format(percent)+"="*strokes_for_bar+"-"*(20-strokes_for_bar)+"] passed {}s, ETA app. {}h {}min {}s         ".format(int(elapsed_time),hours,minutes,seconds), end='\r')
+    global all
+    global elapsed_time
+    global now
+    all+=len(chunk)
+    elapsed_time+=time()-now
+    now=time()
+    percent=all/stream.filesize*100
+    remaining_time=(elapsed_time*100/percent)*((100-percent)/100)
+    hours=int(remaining_time/3600)
+    minutes=int((remaining_time-hours*3600)/60)
+    seconds=int(remaining_time-hours*3600-minutes*60)
+    strokes_for_bar=math.floor(percent//5)
+    print("{:.2f}% [".format(percent)+"="*strokes_for_bar+"-"*(20-strokes_for_bar)+"] passed {}s, ETA app. {}h {}min {}s         ".format(int(elapsed_time),hours,minutes,seconds), end='\r')
